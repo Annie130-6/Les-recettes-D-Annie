7c8901feb5c3d6083e87cddf394fcd344f0637ae
Annie130-6 <a_cleroux@hotmail.com>
2026-09-19 18:54:34 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// RÉGLAGE DES PHOTOS
+// Adresse où se trouvent les anciennes photos (le site "Index-de-mes-recettes").
+// Si les photos changent d'endroit un jour, on modifie seulement cette ligne.
+const IMAGES_BASE = "https://annie130-6.github.io/Index-de-mes-recettes/images/";
