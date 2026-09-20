5fec828e3128062930596c36c37f8c1154dec105
Annie130-6 <a_cleroux@hotmail.com>
2026-09-20 09:15:19 -0400

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// RÉGLAGE DES PHOTOS
-// Adresse où se trouvent les anciennes photos (le site "Index-de-mes-recettes").
-// Si les photos changent d'endroit un jour, on modifie seulement cette ligne.
-const IMAGES_BASE = "https://annie130-6.github.io/Index-de-mes-recettes/images/";
